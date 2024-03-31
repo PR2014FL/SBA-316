c41023abacfab731c74cd6f05d92f17e11c781e4
Josue Morales <157775085+PR2014FL@users.noreply.github.com>
2024-03-31 11:33:55 -0400

    Fully functioning button to add labeled eggs

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 const basket = document.getElementById("eggBasket");
 
 const addAnEgg = function () {
-  let egg = document.createElement("div");
-  egg.setAttribute("class", "egg");
-  basket.appendChild(egg);
-  let eggImg = document.createElement("img");
+  let egg = document.createElement("div"); //Create the .egg div
+  egg.setAttribute("class", "egg"); //add the class "egg"
+  basket.appendChild(egg); //append as child of basket
+
+  let eggImg = document.createElement("img"); //create img tag to sotre the egg pic
   eggImg.setAttribute(
+    //assign the src attribute
     "src",
     "./Assets/Golden-easter-egg-3d-rendering-illustration-on-transparent-background-PNG.png"
   );
-  egg.appendChild(eggImg);
-  let nameLabel = document.createElement("div");
-  nameLabel.setAttribute("class", "eggLabel");
-  egg.appendChild(nameLabel);
-  let pLabel = document.createElement("p");
-  nameLabel.appendChild(pLabel);
-  let labelBtn = document.createElement("button");
-  labelBtn.setAttribute("class", "name");
-  labelBtn.innerHTML("Add Name");
-  egg.appendChild(labelBtn);
-  labelBtn.addEventListener("click", setName());
-  const setName = function () {
-    pLabel.textContent(prompt("Enter Your Name."));
-  };
+  egg.appendChild(eggImg); //append as child of egg
+
+  let nameLabel = document.createElement("div"); //create div for the label
+  nameLabel.setAttribute("class", "eggLabel"); //set the class .eggLabel
+  egg.appendChild(nameLabel); //appened as child of egg div
+
+  let pLabel = document.createElement("p"); //create the p tag
+  nameLabel.appendChild(pLabel); //append p to nameLabel div
+  let textNode = document.createTextNode(prompt("Enter your name.")); //create text node placeholder
+  pLabel.appendChild(textNode); //append text node to p
 };
